1ea942c24f36ccd58a7b6693d3256a70ae08c344
aansubarkah <aansubarkah@gmail.com>
2015-12-09 23:04:59 +0700

    administrator-letters-add component done

---
+++ app/initializers/component-router-injector.js
@@ -0,0 +1,10 @@
+// power all components with router
+
+export function initialize(application) {
+    application.inject('component', 'router', 'router:main');
+}
+
+export default {
+    name: 'component-router-injector',
+    initialize: initialize
+};
--- app/pods/components/administrator-letters/component.js
+++ app/pods/components/administrator-letters/component.js
@@ -4,10 +4,14 @@ export default Ember.Component.extend({
 	title: 'Surat Masuk',
 	searchPlaceholder: 'Search by Perihal',
 	isShowingAdd: false,
+	isShowingView: false,
 	columnsName: ["Tanggal", "Nomor", "Perihal"],
 	actions: {
 		toggleAdd: function() {
 			this.toggleProperty('isShowingAdd');
+		},
+		toggleView: function() {
+			this.toggleProperty('isShowingView');
 		}
 	}
 });
